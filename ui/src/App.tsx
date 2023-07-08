31d18f0ee6a4ed0b9120667549c911e1a488fddc
Afaq Ahmed <136571009+AfaqAhmed-XGrid@users.noreply.github.com>
2023-07-08 17:35:55 +0500

    removed eslint error

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,37 +1,34 @@
 // Import packages
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
 // Import components
-import PrivateRoute from "./ui/components/PrivateRoute";
+import PrivateRoute from './ui/components/PrivateRoute';
 
 // Import pages
-import Signin from "./ui/pages/Signin/Signin";
-import Signup from "./ui/pages/Signup/Signup";
-import ForgotPassword from "./ui/pages/ForgotPassword/ForgotPassword";
-import Dashboard from "./ui/pages/Dashboard/Dashboard";
+import Signin from './ui/pages/Signin/Signin';
+import Signup from './ui/pages/Signup/Signup';
+import Dashboard from './ui/pages/Dashboard/Dashboard';
 
 const App = () => {
-  
-    return (
-      <section>
-        <Routes>
-          <Route
-            path="/dashboard"
-            element={<PrivateRoute Component={Dashboard} />}
-          />
-          <Route
-            path="/"
-            element={<PrivateRoute Component={Dashboard} />}
-          />
-          <Route path="/signin" element=<Signin /> />
-          <Route path="/signup" element=<Signup /> />
-          <Route path="/forgotpassword" element=<ForgotPassword /> />
-        </Routes>
-        <Toaster />
-      </section>
-    );
+  return (
+    <section>
+      <Routes>
+        <Route
+          path="/dashboard"
+          element={<PrivateRoute Component={Dashboard} />}
+        />
+        <Route
+          path="/"
+          element={<PrivateRoute Component={Dashboard} />}
+        />
+        <Route path="/signin" element=<Signin /> />
+        <Route path="/signup" element=<Signup /> />
+      </Routes>
+      <Toaster />
+    </section>
+  );
 };
 
 export default App;
