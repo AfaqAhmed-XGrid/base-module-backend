ffaf7a4a8195a5b63690184dfff9981c0a0ee744
Afaq Ahmed <136571009+AfaqAhmed-XGrid@users.noreply.github.com>
2023-07-10 10:51:50 +0500

    removed unwanted files and made navbar responsive

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,16 +1,10 @@
 // Import packages
 import React from 'react';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 
 // Import components
-import PrivateRoute from './ui/components/PrivateRoute';
 import Navbar from './ui/components/Navbar/Navbar';
-
-// Import pages
-import Signin from './ui/pages/Signin/Signin';
-import Signup from './ui/pages/Signup/Signup';
-import Dashboard from './ui/pages/Dashboard/Dashboard';
 import SideNavbar from './ui/components/SideNavbar/SideNavbar';
 
 const App = () => {
@@ -20,18 +14,6 @@ const App = () => {
     <section>
       {showNavbar && <Navbar />}
       {showNavbar && <SideNavbar />}
-      <Routes>
-        <Route
-          path="/dashboard"
-          element={<PrivateRoute Component={Dashboard} />}
-        />
-        <Route
-          path="/"
-          element={<PrivateRoute Component={Dashboard} />}
-        />
-        <Route path="/signin" element=<Signin /> />
-        <Route path="/signup" element=<Signup /> />
-      </Routes>
       <Toaster />
     </section>
   );
--- ui/src/ui/components/Navbar/Navbar.tsx
+++ ui/src/ui/components/Navbar/Navbar.tsx
@@ -1,8 +1,15 @@
 // Import packages
 import React, { useEffect, useState } from 'react';
+import { toast } from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 // Import react icons
 import { FaUserCircle } from 'react-icons/fa';
+import { BsFillInfoCircleFill } from 'react-icons/bs';
+import { AiOutlineLogout } from 'react-icons/ai';
+
+// Import rtk query
+import { useLogoutUserMutation } from '../../../store/api';
 
 // Import css
 import './Navbar.css';
@@ -18,6 +25,9 @@ const Navbar = () => {
   const [showProfile, setShowProfile] = useState(false);
   const [user, setUser] = useState<User | undefined>(undefined);
   const authStatus = useAuthStatus();
+  const navigate = useNavigate();
+
+  const [logoutUser] = useLogoutUserMutation();
 
   useEffect(() => {
     const fetchProfileData = () => {
@@ -27,24 +37,105 @@ const Navbar = () => {
     fetchProfileData();
   }, [authStatus]);
 
+  /**
+   * Function logout user
+   */
+  const onLogout = async () => {
+    const res = await logoutUser(null);
+    const response =
+      'data' in res ? res.data : 'data' in res.error ? res.error.data : null;
+
+    if (response.success) {
+      toast.success(`${response.message}`, {
+        duration: 3000,
+        position: 'bottom-center',
+        ariaProps: {
+          'role': 'status',
+          'aria-live': 'polite',
+        },
+      });
+      navigate('/signin');
+    } else {
+      toast.error(`${response.message}`, {
+        duration: 3000,
+        position: 'bottom-center',
+        ariaProps: {
+          'role': 'status',
+          'aria-live': 'polite',
+        },
+      });
+    }
+  };
+
   return (
     <div className="nav-main-container">
       <div style={{ backgroundColor: '#01645F' }}>
         <img src="/assets/logo.png" alt="" className="logo" />
       </div>
-      <h2 className='nav-title'>
-      Cine-Info:  Connecting You to the Movie Universe
+      <h2 className="nav-title">
+        Cine-Info: Connecting You to the Movie Universe
       </h2>
       {user?.profilePicture ? (
-          <div onClick={() => setShowProfile(!showProfile)}>
-            <img src={user?.profilePicture} alt="" className="profile-image" />
-          </div>
-        ) : (
-          <FaUserCircle
-            className='profile-icon'
-            onClick={() => setShowProfile(!showProfile)}
+        <div onClick={() => setShowProfile(!showProfile)}>
+          <img
+            src={user?.profilePicture}
+            alt=""
+            className="nav-profile-image"
           />
-        )}
+        </div>
+      ) : (
+        <FaUserCircle
+          className="nav-profile-icon"
+          onClick={() => setShowProfile(!showProfile)}
+        />
+      )}
+      {showProfile && (
+        <>
+          <div
+            className="nav-profile-main-container"
+            onClick={() => setShowProfile(false)}
+          ></div>
+          <div className="nav-profile-container">
+            {user?.profilePicture ? (
+              <img
+                src={user?.profilePicture}
+                alt=""
+                className="profile-image"
+              />
+            ) : (
+              <FaUserCircle
+                className="profile-icon"
+                onClick={() => setShowProfile(!showProfile)}
+              />
+            )}
+            <p className="profile-displayName">{user?.displayName}</p>
+            <div className="hover">
+              <div className="profile-info-button">
+                <BsFillInfoCircleFill className="profile-info-button-icon" />
+                Info
+              </div>
+              <div className="profile-info-container">
+                <table className='info-table'>
+                  <tr className='table-head-column'>
+                    <td className='table-heading'>Email: </td>
+                    <td className='table-content'>{user?.email}</td>
+                  </tr>
+                  <tr className='table-content-column'>
+                    <td className='table-heading'>Name: </td>
+                    <td className='table-content'>{user?.displayName}</td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+            <div className="profile-info-button" onClick={onLogout}>
+              <AiOutlineLogout
+                className="profile-info-button-icon"
+              />
+              logout
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
